bdd9a7049453c5e386752eb5918e914e007646cf
CollinsWong <collinswong98@gmail.com>
2023-07-26 08:06:05 -0700

    Applied hamburger menu and toggle functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 console.log ("Welcome to my page!")
+var person = {
+    name: "Collins",
+    age: 24,
+    hobbies: "basketball",
+}
+
+console.log (person)
+
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
